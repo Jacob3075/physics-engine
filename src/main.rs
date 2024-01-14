1a6dfc1eb1b43c9c717d1ba9e0796d0cd23ab2bc
Jacob <jacob.bosco3075@gmail.com>
2024-01-14 22:21:00 +0530

    added some shapes

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod ui;
 
 use crate::ui::{cursor_coords_system, setup_ui, CursorCoords};
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
 
 fn main() {
     App::new().add_plugins((DefaultPlugins, EngineSetup)).run();
@@ -14,6 +15,49 @@ impl Plugin for EngineSetup {
     fn build(&self, app: &mut App) {
         app.init_resource::<CursorCoords>();
         app.add_systems(Startup, setup_ui);
+        app.add_systems(Startup, setup_objects);
         app.add_systems(Update, cursor_coords_system);
     }
+}
+fn setup_objects(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Circle
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        ..default()
+    });
+
+    // Rectangle
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 100.0)),
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+        ..default()
+    });
+
+    // Quad
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes
+            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
+            .into(),
+        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
+        transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
+        ..default()
+    });
+
+    // Hexagon
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
+        transform: Transform::from_translation(Vec3::new(150., 0., 0.)),
+        ..default()
+    });
 }
