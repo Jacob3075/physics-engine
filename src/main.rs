499f1d9b6eba1b8304dc5b7a31e264b934678ada
Jacob Bosco <jacob.bosco3075@gmail.com>
2024-01-14 15:43:53 +0530

    setup simple UI to show mouse coords

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+mod components;
+mod ui;
+
+use crate::ui::{cursor_coords_system, setup_ui, CursorCoords};
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new().add_plugins((DefaultPlugins, EngineSetup)).run();
+}
+
+pub struct EngineSetup;
+
+impl Plugin for EngineSetup {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<CursorCoords>();
+        app.add_systems(Startup, setup_ui);
+        app.add_systems(Update, cursor_coords_system);
+    }
 }
---
+++ src/ui.rs
@@ -0,0 +1,58 @@
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+/// We will store the world position of the mouse cursor here.
+#[derive(Resource, Default)]
+pub struct CursorCoords(pub Vec2);
+
+/// Used to help identify our main camera
+#[derive(Component)]
+pub struct MainCamera;
+
+#[derive(Component)]
+pub struct FpsText;
+
+pub fn cursor_coords_system(
+    mut my_coords: ResMut<CursorCoords>,
+    q_window: Query<&Window, With<PrimaryWindow>>,
+    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    mut q_coords_text: Query<&mut Text, With<FpsText>>,
+) {
+    let (camera, camera_transform) = q_camera.single();
+    let window = q_window.single();
+    let mut coords_text = q_coords_text.single_mut();
+
+    if let Some(world_position) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        if my_coords.0 == world_position {
+            return;
+        };
+        my_coords.0 = world_position;
+
+        coords_text.sections[1].value = format!("{:.1}/{:.1}", world_position.x, world_position.y);
+    }
+}
+
+pub fn setup_ui(mut commands: Commands) {
+    commands.spawn((Camera2dBundle::default(), MainCamera));
+    commands.spawn((
+        TextBundle::from_sections([
+            TextSection::new(
+                "Coords: ",
+                TextStyle {
+                    font_size: 60.0,
+                    ..default()
+                },
+            ),
+            TextSection::from_style(TextStyle {
+                font_size: 60.0,
+                color: Color::GOLD,
+                ..default()
+            }),
+        ]),
+        FpsText,
+    ));
+}
